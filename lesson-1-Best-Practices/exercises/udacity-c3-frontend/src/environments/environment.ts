28c19610c771582f9c767f25d93f088ea5fd3d50
Patrik Björk <patrik.bjork@codespirit.se>
2020-04-28 10:40:09 +0200

    Add my cloud environment (commented out).

--- lesson-1-Best-Practices/exercises/udacity-c3-frontend/src/environments/environment.ts
+++ lesson-1-Best-Practices/exercises/udacity-c3-frontend/src/environments/environment.ts
@@ -8,6 +8,12 @@ export const environment = {
   apiHost: 'http://localhost:8080/api/v0'
 };
 
+/*export const environment = {
+  production: false,
+  appName: 'Patrik Udagram',
+  apiHost: 'http://udagram-patrik-dev-dev.eu-north-1.elasticbeanstalk.com/api/v0'
+};*/
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
