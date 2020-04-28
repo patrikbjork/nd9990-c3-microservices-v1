c0ac019d3dd30cc78ee1f779206fbd98ebb80c4a
Patrik Björk <patrik.bjork@codespirit.se>
2020-04-28 10:24:55 +0200

    Change/fix config.

--- lesson-1-Best-Practices/exercises/udacity-c3-restapi/src/config/config.ts
+++ lesson-1-Best-Practices/exercises/udacity-c3-restapi/src/config/config.ts
@@ -1,23 +1,17 @@
 export const config = {
-  "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
-    "host": process.env.POSTGRESS_HOST,
-    "dialect": "postgres",
-    "aws_reigion": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET
+  'postgres': {
+    'username': process.env.UDAGRAM_POSTGRES_USER,
+    'password': process.env.UDAGRAM_POSTGRES_PASSWORD,
+    'database': process.env.UDAGRAM_POSTGRES_DATABASE,
+    'host': process.env.UDAGRAM_POSTGRES_HOST,
+    'dialect': 'postgres'
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+  'aws': {
+    'aws_region': process.env.UDAGRAM_AWS_REGION,
+    'aws_profile': process.env.UDAGRAM_AWS_PROFILE,
+    'aws_media_bucket': process.env.UDAGRAM_AWS_MEDIA_BUCKET
   },
-  "jwt": {
-    "secret": process.env.JWT_SECRET
+  'jwt': {
+    'secret': process.env.UDAGRAM_JWT_SECRET
   }
-
-}
+};
